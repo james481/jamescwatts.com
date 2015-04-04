da32eae65edc7a12db1223716bf878a9d4ac00cd
James Watts <jamescwatts@gmail.com>
2015-04-04 11:16:49 -0400

    Fix map type setting, recenter

--- web/js/grayscale.js
+++ web/js/grayscale.js
@@ -41,7 +41,7 @@ function init() {
         zoom: 15,
 
         // The latitude and longitude to center the map (always required)
-        center: new google.maps.LatLng(40.0067,-105.2998), // New York
+        center: new google.maps.LatLng(40.0144,-105.3160),
 
         // Disables the default Google Maps UI components
         disableDefaultUI: true,
@@ -49,7 +49,7 @@ function init() {
         draggable: false,
 
         // Show Terrain
-        mapTypeId: 'TERRAIN',
+        mapTypeId: google.maps.MapTypeId.TERRAIN,
 
         // How you would like to style the map.
         // This is where you would paste any style found on Snazzy Maps.
@@ -172,7 +172,7 @@ function init() {
 
     // Custom Map Marker Icon - Customize the map-marker.png file to customize your icon
     var image = 'img/map-marker.png';
-    var myLatLng = new google.maps.LatLng(40.0067,-105.2998);
+    var myLatLng = new google.maps.LatLng(40.0144,-105.3160);
     var beachMarker = new google.maps.Marker({
         position: myLatLng,
         map: map,
